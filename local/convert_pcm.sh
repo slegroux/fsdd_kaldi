73399cbdca47c25c517bafbad1477a649ac5a24e
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-02-27 23:32:45 -0800

    makes sure data has right pcm format

---
+++ local/convert_pcm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+wav_folder=$1
+
+for file in $wav_folder/*.wav; do
+  echo $file
+  encoding=$(soxi -e $file)
+  bitrate=$(soxi -b $file)
+  channels=$(soxi -c $file)
+  if [ "$encoding" != "Signed Integer PCM" ] || [ "$channels" != 1 ] || [ "$bitrate" != 16 ]; then
+      echo "current .wav: " $bitrate bit $encoding $channels channels $file
+      sox $file -b 16 -c 1 -e 'signed-integer' tmp.wav
+      cp $file $file.~
+      mv tmp.wav $file
+  fi
+
+done
+
+
+
