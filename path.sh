966f3165b565489ae69c00bb48242233b2aeed9a
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-03-05 17:39:41 -0800

    add cmvn-online

--- path.sh
+++ path.sh
@@ -1,6 +1,6 @@
 export KALDI_ROOT=`pwd`/../..
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-
+. $KALDI_ROOT/tools/config/common_path.sh
 # Setting paths to useful tools
 export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lmbin/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
 
