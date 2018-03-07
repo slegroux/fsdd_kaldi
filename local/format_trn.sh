ed022dd122177b39b2f9606b6fbb3737b6d3674d
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-03-06 18:23:15 -0800

    formatted transcripts

--- local/format_trn.sh
+++ local/format_trn.sh
@@ -2,17 +2,23 @@
 
 wav_folder=$1
 
-for file in $wav_folder/*.wav; do
-  encoding=$(soxi -e $file)
-  bitrate=$(soxi -b $file)
-  channels=$(soxi -c $file)
-  if [ "$encoding" != "Signed Integer PCM" ] || [ "$channels" != 1 ] || [ "$bitrate" != 16 ]; then
-      echo "current .wav: " $bitrate bit $encoding $channels channels $file
-      sox $file -b 16 -c 1 -e 'signed-integer' tmp.wav
-      cp $file $file.~
-      mv tmp.wav $file
-  fi
+declare -a dict
+dict[0]=zero
+dict[1]=one
+dict[2]=two
+dict[3]=three
+dict[4]=four
+dict[5]=five
+dict[6]=six
+dict[7]=seven
+dict[8]=eight
+dict[9]=nine
 
+for file in $wav_folder/*.wav; do
+    id=$(basename $file .wav)
+    digit=$(echo $(basename $file) | cut -d'_' -f1)
+    speaker=$(echo $(basename $file) | cut -d'_' -f2)
+    echo ${speaker}_${id} ${dict[$digit]} > $wav_folder/$(basename $file .wav)".ref.txt"
 done
 
 
