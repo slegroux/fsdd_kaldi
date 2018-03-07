dd0fce496e913923a805975e2ade7a111a2140c8
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-03-06 18:22:38 -0800

    add folder input

--- local/convert_pcm.sh
+++ local/convert_pcm.sh
@@ -8,9 +8,9 @@ for file in $wav_folder/*.wav; do
   bitrate=$(soxi -b $file)
   channels=$(soxi -c $file)
   if [ "$encoding" != "Signed Integer PCM" ] || [ "$channels" != 1 ] || [ "$bitrate" != 16 ]; then
-      echo "current .wav: " $bitrate bit $encoding $channels channels $file
+      echo "converting current .wav: " $bitrate bit $encoding $channels channels $file
       sox $file -b 16 -c 1 -e 'signed-integer' tmp.wav
-      cp $file $file.~
+      cp $file $file~
       mv tmp.wav $file
   fi
 
