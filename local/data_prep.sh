fb82ef3eb1acab0baddbcdd121df1b3fe92c529f
Sylvain Le Groux <sylvainlg@voicera.ai>
2018-03-07 16:06:26 -0800

    wrap up data prep scripts

--- local/data_prep.sh
+++ local/data_prep.sh
@@ -2,11 +2,16 @@
 
 data_dir=$DATA/free-spoken-digit-dataset/recordings
 
-# check and convert audio format 
-./convert_pcm.sh $data_dir
+echo "==== check and convert audio format "
+#./convert_pcm.sh $data_dir || exit 1
 
-# create formatted transcript for each utterance
-./format_trn.sh $data_dir
+echo "==== create formatted transcript for each utterance"
+#./format_trn.sh $data_dir || exit 1
 
-# create train/test split
-./train_test_split.sh $data_dir
+echo "==== create train/test split"
+rm -rf $data_dir/train $data_dir/test
+mkdir -p $data_dir/train $data_dir/test
+./train_test_split.sh $data_dir || exit 1
+
+echo "==== generate wav.scp & utt2spk"
+./create_wav_scp_utt2spk.sh $data_dir || exit 1
